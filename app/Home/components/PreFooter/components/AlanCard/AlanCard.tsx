4ae342bd9bb814fc2aea1542cbfc81854445ad55
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-01 01:44:26 -0500

    feat: :art: zoom img
    Tuti Img Zoom de alan
    
    [dev]

--- app/Home/components/PreFooter/components/AlanCard/AlanCard.tsx
+++ app/Home/components/PreFooter/components/AlanCard/AlanCard.tsx
@@ -1,6 +1,4 @@
-//import Alan from '@app/assets/img/AlanBuscaglia.png';
 import { TutiImgZoom } from '@app/common';
-//import Image from 'next/image';
 
 import styles from './styles/alanCard.module.css';
 
@@ -8,11 +6,6 @@ export const AlanCard = () => {
   return (
     <article className={styles.footer__alanCard}>
       <TutiImgZoom />
-      {/* <Image
-        className={styles.footer__alanImg}
-        src={Alan}
-        alt="foto de Alan Buscaglia"
-      /> */}
       <section className={styles.footer__textContainer}>
         <header className={styles.footer__alanTitle}>Alan Buscaglia</header>
         <p>Ingeniero y arquitecto de Software Front-end</p>
